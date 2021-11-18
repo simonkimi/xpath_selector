ce6a937e387870cd05c3a730992684e9695e6e02
simonkimi <805757448@qq.com>
2021-11-18 22:54:27 +0800

    Add XPathElement selector

--- lib/src/ext.dart
+++ lib/src/ext.dart
@@ -1,5 +1,7 @@
 import 'package:html/dom.dart';
 import 'package:xml/xml.dart';
+import 'package:xpath_selector/src/model/html.dart';
+import 'package:xpath_selector/src/model/xml.dart';
 import 'builder.dart';
 
 extension ElementHelper on Element {
@@ -10,4 +12,16 @@ extension ElementHelper on Element {
 extension XmlElementHelper on XmlElement {
   /// Xml XPath query
   XPathResult queryXPath(String xpath) => XPath.xmlElement(this).query(xpath);
+}
+
+extension XmlDocumentHelper on XmlElementTree {
+  /// Xml XPath query
+  XPathResult queryXPath(String xpath) =>
+      XPath.xmlElement(element).query(xpath);
+}
+
+extension HtmlDocumentHelper on HtmlElementTree {
+  /// Xml XPath query
+  XPathResult queryXPath(String xpath) =>
+      XPath.htmlElement(element).query(xpath);
 }
